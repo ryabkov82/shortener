b822a8d5b2ad7b4288470ac5983eb2194511fe16
Sergey.Ryabkov <Sergey.Ryabkov@russianpost.ru>
2025-03-02 21:45:34 +0300

    51

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -3,7 +3,9 @@ package config
 import (
 	"errors"
 	"flag"
+	"log"
 	"net/url"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -13,43 +15,75 @@ type Config struct {
 	BaseURL        string
 }
 
+func validateHTTPServerAddr(addr string) error {
+
+	hp := strings.Split(addr, ":")
+	if len(hp) != 2 {
+		return errors.New("need address in a form host:port")
+	}
+	_, err := strconv.Atoi(hp[1])
+
+	return err
+}
+
+func validateBaseURL(baseURL string) error {
+
+	_, err := url.Parse(baseURL)
+
+	return err
+}
+
 func Load() *Config {
 
 	cfg := new(Config)
 	cfg.HTTPServerAddr = "localhost:8080"
 	cfg.BaseURL = "http://localhost:8080/"
 
-	/*
-		flag.StringVar(&cfg.HTTPServerAddr, "a", "localhost:8080", "Server address host:port")
-		flag.StringVar(&cfg.BaseURL, "b", "localhost:8080", "Base address shortened url")
-	*/
-
 	flag.Func("a", "Server address host:port", func(flagValue string) error {
 
-		hp := strings.Split(flagValue, ":")
-		if len(hp) != 2 {
-			return errors.New("need address in a form host:port")
-		}
-		_, err := strconv.Atoi(hp[1])
+		err := validateHTTPServerAddr(flagValue)
+
 		if err != nil {
 			return err
 		}
+
 		cfg.HTTPServerAddr = flagValue
 		return nil
 	})
 
 	flag.Func("b", "Base address shortened url", func(flagValue string) error {
 
-		_, err := url.Parse(flagValue)
+		err := validateBaseURL(flagValue)
 
 		if err != nil {
 			return err
 		}
+
 		cfg.BaseURL = flagValue
 		return nil
 	})
 
 	flag.Parse()
+
+	if envHTTPServerAddr := os.Getenv("SERVER_ADDRESS"); envHTTPServerAddr != "" {
+
+		err := validateHTTPServerAddr(envHTTPServerAddr)
+		if err != nil {
+			log.Fatalf("error validate SERVER_ADDRESS: %s", err)
+		}
+
+		cfg.HTTPServerAddr = envHTTPServerAddr
+	}
+
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+
+		err := validateBaseURL(envBaseURL)
+		if err != nil {
+			log.Fatalf("error validate BASE_URL: %s", err)
+		}
+
+		cfg.BaseURL = envBaseURL
+	}
 
 	return cfg
 
