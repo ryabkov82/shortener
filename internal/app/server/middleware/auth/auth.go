c06c3aad610d7d41b6c8e1f3d62ecb98fb2c6de8
Sergey.Ryabkov <Sergey.Ryabkov@russianpost.ru>
2025-04-14 01:46:00 +0300

    14-8

--- internal/app/server/middleware/auth/auth.go
+++ internal/app/server/middleware/auth/auth.go
@@ -17,9 +17,8 @@ func JWTAutoIssue(jwtKey []byte) func(next http.Handler) http.Handler {
 
 			cookie, err := r.Cookie("token")
 			if err != nil || cookie == nil {
-				userID := issueNewToken(w, jwtKey)
-				//ctx := context.WithValue(r.Context(), jwtauth.UserIDContextKey, jwtauth.UserDefault) // для поддержки обратной совместимости
-				ctx := context.WithValue(r.Context(), jwtauth.UserIDContextKey, userID)
+				//userID := issueNewToken(w, jwtKey)
+				ctx := context.WithValue(r.Context(), jwtauth.UserIDContextKey, jwtauth.UserDefault) // для поддержки обратной совместимости
 				r = r.WithContext(ctx)
 				next.ServeHTTP(w, r)
 				return
@@ -36,9 +35,8 @@ func JWTAutoIssue(jwtKey []byte) func(next http.Handler) http.Handler {
 			})
 
 			if err != nil || !token.Valid {
-				userID := issueNewToken(w, jwtKey)
-				//ctx := context.WithValue(r.Context(), jwtauth.UserIDContextKey, jwtauth.UserDefault) // для поддержки обратной совместимости
-				ctx := context.WithValue(r.Context(), jwtauth.UserIDContextKey, userID)
+				//userID := issueNewToken(w, jwtKey)
+				ctx := context.WithValue(r.Context(), jwtauth.UserIDContextKey, jwtauth.UserDefault) // для поддержки обратной совместимости
 				r = r.WithContext(ctx)
 				next.ServeHTTP(w, r)
 				return
@@ -65,7 +63,7 @@ func StrictJWTAutoIssue(jwtKey []byte) func(next http.Handler) http.Handler {
 			cookie, err := r.Cookie("token")
 			if err != nil || cookie == nil {
 				_ = issueNewToken(w, jwtKey)
-				http.Error(w, "New token issued", http.StatusUnauthorized)
+				http.Error(w, "Status unauthorized", http.StatusUnauthorized)
 				return
 			}
 
@@ -81,7 +79,7 @@ func StrictJWTAutoIssue(jwtKey []byte) func(next http.Handler) http.Handler {
 
 			if err != nil || !token.Valid {
 				_ = issueNewToken(w, jwtKey)
-				http.Error(w, "New token issued", http.StatusUnauthorized)
+				http.Error(w, "Status unauthorized", http.StatusUnauthorized)
 				return
 			}
 
--- internal/app/storage/inmemory/inmemory.go
+++ internal/app/storage/inmemory/inmemory.go
@@ -138,10 +138,9 @@ func (s *InMemoryStorage) GetRedirectURL(ctx context.Context, shortKey string) (
 		return models.URLMapping{}, storage.ErrURLNotFound
 	}
 
-	// закомментировал чтобы пройти автотесты
-	//if url.UserID != userID {
-	//	return models.URLMapping{}, storage.ErrURLNotFound
-	//}
+	if url.UserID != userID {
+		return models.URLMapping{}, storage.ErrURLNotFound
+	}
 
 	mapping := models.URLMapping{
 		ShortURL:    url.ShortURL,
