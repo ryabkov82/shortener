78028e9d5c64bdefcd8dc66a1b2d01e9dba589d7
Sergey.Ryabkov <Sergey.Ryabkov@russianpost.ru>
2025-04-14 11:34:48 +0300

    14-11

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -52,24 +52,37 @@ func StartServer(log *zap.Logger, cfg *config.Config) {
 	router.Use(mwlogger.RequestLogging(log))
 	router.Use(mwgzip.Gzip)
 
-	// Группа с автоматической аутентификацией
-	router.Group(func(router chi.Router) {
-		router.Use(auth.JWTAutoIssue([]byte(cfg.JwtKey)))
+	/*
+		// Группа с автоматической аутентификацией
+		router.Group(func(router chi.Router) {
+			router.Use(auth.JWTAutoIssue([]byte(cfg.JwtKey)))
 
-		router.Post("/", shorturl.GetHandler(srv, cfg.BaseURL, log))
-		router.Get("/{id}", redirect.GetHandler(srv, log))
+			router.Post("/", shorturl.GetHandler(srv, cfg.BaseURL, log))
+			router.Get("/{id}", redirect.GetHandler(srv, log))
 
-		router.Post("/api/shorten", shortenapi.GetHandler(srv, cfg.BaseURL, log))
+			router.Post("/api/shorten", shortenapi.GetHandler(srv, cfg.BaseURL, log))
 
-		router.Get("/ping", ping.GetHandler(srv, log))
-		router.Post("/api/shorten/batch", batch.GetHandler(srv, cfg.BaseURL, log))
-	})
+			router.Get("/ping", ping.GetHandler(srv, log))
+			router.Post("/api/shorten/batch", batch.GetHandler(srv, cfg.BaseURL, log))
+		})
 
-	// Группа со строгой аутентификацией
-	router.Group(func(router chi.Router) {
-		router.Use(auth.StrictJWTAutoIssue([]byte(cfg.JwtKey)))
-		router.Get("/api/user/urls", userurls.GetHandler(srv, cfg.BaseURL, log))
-	})
+		// Группа со строгой аутентификацией
+		router.Group(func(router chi.Router) {
+			router.Use(auth.StrictJWTAutoIssue([]byte(cfg.JwtKey)))
+			router.Get("/api/user/urls", userurls.GetHandler(srv, cfg.BaseURL, log))
+		})
+	*/
+
+	router.Use(auth.JWTAutoIssue([]byte(cfg.JwtKey)))
+
+	router.Post("/", shorturl.GetHandler(srv, cfg.BaseURL, log))
+	router.Get("/{id}", redirect.GetHandler(srv, log))
+
+	router.Post("/api/shorten", shortenapi.GetHandler(srv, cfg.BaseURL, log))
+
+	router.Get("/ping", ping.GetHandler(srv, log))
+	router.Post("/api/shorten/batch", batch.GetHandler(srv, cfg.BaseURL, log))
+	router.Get("/api/user/urls", userurls.GetHandler(srv, cfg.BaseURL, log))
 
 	log.Info("Server started", zap.String("address", cfg.HTTPServerAddr))
 
